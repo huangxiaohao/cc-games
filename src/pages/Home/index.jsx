a00d196ec06318749fbd1506a301cd2c84652591
北海 <huangyuehao@cai-inc.com>
2026-04-25 22:18:56 +0800

    feat: mock

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,12 +8,22 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <img src={bgHome} alt="cc-games" className={styles.img} />
+      <p className={styles.info}>XXXXXXXXXXXXXXXXXXX</p>
 
       {/* 点击热区 - 智绘新章入口 */}
-      <div
-        className={styles.hotArea1}
-        onClick={() => navigate('/smartDraw')}
-      />
+      <div className={styles.hotArea1} onClick={() => navigate("/smartDraw")} />
+
+      {/* 点击热区2 */}
+      <div className={styles.hotArea2} onClick={() => alert("点击热区2")} />
+
+      {/* 点击热区3 */}
+      <div className={styles.hotArea3} onClick={() => alert("点击热区3")} />
+
+      {/* 点击热区4 */}
+      <div className={styles.hotArea4} onClick={() => alert("点击热区4")} />
+
+      {/* 点击热区5 */}
+      <div className={styles.hotArea5} onClick={() => alert("点击热区5")} />
     </div>
   );
 }
