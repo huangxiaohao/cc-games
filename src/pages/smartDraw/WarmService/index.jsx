b34822d833b1a8d4dfb14fef44ccd012f8e0ee7b
北海 <huangyuehao@cai-inc.com>
2026-04-27 18:49:53 +0800

    ``` feat(WarmService): 添加热区点击功能
    添加了三个可点击的热区区域，用于跳转到外部调研和服务评价页面，
    提升用户体验。新增了 react-router-dom 的 useNavigate 钩子用于页面导航，
    并在样式文件中定义了相应热区的定位和尺寸。
    ```

--- src/pages/smartDraw/WarmService/index.jsx
+++ src/pages/smartDraw/WarmService/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './index.module.css';
 import BackButton from '../../../components/BackButton';
 import bgWarmService from "../../../assets/smartDraw/01-01.png";
@@ -7,6 +8,7 @@ import { getWarmServiceList } from '../../../services/api';
 /* 暖心服务 */
 
 export default function WarmService() {
+  const navigate = useNavigate();
   const [list, setList] = useState([]);
 
   useEffect(() => {
@@ -20,9 +22,44 @@ export default function WarmService() {
         <img src={bgWarmService} alt="让服务升温" className={styles.img} />
         <div className={styles.info}>
           {list.map((item, index) => (
-            <div key={index}>{item.name}　{item.score}积分</div>
+            <div key={index}>
+              {item.name}　{item.score}积分
+            </div>
           ))}
         </div>
+
+        {/* 点击热区1 */}
+        <div
+          className={styles.hotArea1}
+          onClick={() =>
+            window.open(
+              "https://survey.caocaokeji.cn/survey/diaowen.html?sid=lyh5guqt6",
+              "_blank",
+            )
+          }
+        />
+
+        {/* 点击热区2 */}
+        <div
+          className={styles.hotArea2}
+          onClick={() =>
+            window.open(
+              "https://survey.caocaokeji.cn/survey/diaowen.html?sid=r37k52xx",
+              "_blank",
+            )
+          }
+        />
+
+        {/* 点击热区3 */}
+        <div
+          className={styles.hotArea3}
+          onClick={() =>
+            window.open(
+              "https://mobile.caocaokeji.cn/driver-review/customer-service/login?isServiceCheck=true",
+              "_blank",
+            )
+          }
+        />
       </div>
     </div>
   );
